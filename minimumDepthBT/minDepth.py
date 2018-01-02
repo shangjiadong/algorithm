a1a3efffc073be49371f257ffd5870548d1299c1
Shangjia Dong <shanga.dong@gmail.com>
2018-01-01 18:12:08 -0800

    function added

--- minimumDepthBT/minDepth.py
+++ minimumDepthBT/minDepth.py
@@ -24,4 +24,61 @@ class Solution(object):
             return 1 + self.minDepth(root.left)
         return min(self.minDepth(root.left), self.minDepth(root.right))+1
         
+def stringToTreeNode(input):
+    input = input.strip()
+    input = input[1:-1]
+    if not input:
+        return None
+
+    inputValues = [s.strip() for s in input.split(',')]
+    root = TreeNode(int(inputValues[0]))
+    nodeQueue = [root]
+    front = 0
+    index = 1
+    while index < len(inputValues):
+        node = nodeQueue[front]
+        front = front + 1
+
+        item = inputValues[index]
+        index = index + 1
+        if item != "null":
+            leftNumber = int(item)
+            node.left = TreeNode(leftNumber)
+            nodeQueue.append(node.left)
+
+        if index >= len(inputValues):
+            break
+
+        item = inputValues[index]
+        index = index + 1
+        if item != "null":
+            rightNumber = int(item)
+            node.right = TreeNode(rightNumber)
+            nodeQueue.append(node.right)
+    return root
+
+def intToString(input):
+    if input is None:
+        input = 0
+    return str(input)
+
+def main():
+    import sys
+    def readlines():
+        for line in sys.stdin:
+            yield line.strip('\n')
+    lines = readlines()
+    while True:
+        try:
+            line = lines.next()
+            root = stringToTreeNode(line)
             
+            ret = Solution().maxDepth(root)
+
+            out = intToString(ret)
+            print out
+        except StopIteration:
+            break
+
+if __name__ == '__main__':
+    main()            
