b07818e965331f5ea0646a9c45d0e63ddfe31240
Shangjia Dong <shanga.dong@gmail.com>
2018-02-05 22:27:04 -0800

    k smallest element with heapq

--- ksmall_w_heap/ksmallest.py
+++ ksmall_w_heap/ksmallest.py
@@ -12,37 +12,23 @@ a) it should work with both lists and lazy lists
 b) the output list should be sorted
 """
 
-def maxHeapify(alist, n, i):
-    largest = i
-    l = 2*i + 1
-    r = 2*i + 2
-    if (l < n) and alist[l] > alist[largest]:
-        largest = l
-    if (r < n) and alist[r] > alist[largest]:
-        largest = r
-    if largest != i:
-        alist[i], alist[largest] = alist[largest], alist[i]
-        maxHeapify(alist, n, largest)
-def heapSort(aHeap):
-    len_heap = len(aHeap)-1
-    for n in range(len_heap, -1, -1):
-        aHeap[0], aHeap[n] = aHeap[n], aHeap[0]
-        maxHeapify(aHeap, n, 0)
-
+from heapq import *
 def ksmallest(k, alist):
-    blist = list(alist[:k])
-    for item in list(alist[k:]):
-        if item < blist[0]:
-            blist[0] = item
-            for j in range(k-1, -1, -1):
-                maxHeapify(blist, len(blist), j)
-    heapSort(blist)
-    return blist
+    # if k < 1: return []
+    if k > len(alist):
+        k = len(alist)
+    heap = []
+    for idx, val in enumerate(alist):
+        if idx < k:
+            heappush(heap, -val)
+        else:
+            if -val > heap[0]:
+                heapreplace(heap, -val)
+    res = [-heappop(heap) for i in range(len(heap))] 
+    return res[::-1]
 
 def main():
-	print(ksmallest(4, [10, 2, 9, 3, 7, 8, 11, 5, 7]))
-
+    print(ksmallest(11, [10, 2, 9, 3, 7, 8, 11, 5, 7]))
 
 if __name__ == '__main__':
-	main()
-
+    main()
